7747a9ae10ce6030841bed179e8caa359e53d1e8
how8621 <how8621@gmail.com>
2020-12-27 22:56:49 +0900

    Version up Angular 8 to 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostAddComponent } from './post-add/post-add.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxIndexedDBModule, DBConfig } from 'ngx-indexed-db';
+import { MenuListComponent } from './menu-list/menu-list.component';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+const dbConfig: DBConfig = {
+    name: 'pLogDB',
+    version: 1,
+    objectStoresMeta: [
+        {
+            store: 'menu',
+            storeConfig: { keyPath: 'id', autoIncrement: true },
+            storeSchema: [
+                { name: 'name', keypath: 'name', options: { unique: false} }
+            ]
+        },
+        {
+            store: 'post',
+            storeConfig: { keyPath: 'id', autoIncrement: true },
+            storeSchema: [
+                { name: 'title', keypath: 'title', options: { unique: false } },
+                { name: 'note', keypath: 'note', options: { unique: false } },
+                { name: 'menu', keypath: 'menu', options: { unique: false } },
+            ]
+        }
+    ]
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PostAddComponent,
+        MenuListComponent
+    ],
+    imports: [
+        NgxIndexedDBModule.forRoot(dbConfig),
+        FormsModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatListModule,
+        MatIconModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
